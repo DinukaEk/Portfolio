146514d43458194a6e5d55cf14d74b63aea4f01d
Dinuka Ekanayake <79810886+DinukaEk@users.noreply.github.com>
2023-01-01 18:45:32 +0530

    v1.4

--- js/main.js
+++ js/main.js
@@ -182,3 +182,22 @@ $(window).load(function(){
   });
 
 })
+
+// ========================================================================= //
+//  Skills
+// ========================================================================= //
+
+let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
