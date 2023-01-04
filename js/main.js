0e11b77c4a94b44c413559d379f17f409cb4887b
Dinuka Ekanayake <79810886+DinukaEk@users.noreply.github.com>
2023-01-04 17:52:12 +0530

    v1.5

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ $(document).ready(function() {
   // Call the functions
   magnifPopup();
 
-});
+
 
 // ========================================================================= //
 //  Porfolio isotope and filter
@@ -187,17 +187,49 @@ $(window).load(function(){
 //  Skills
 // ========================================================================= //
 
-let skilsContent = select('.skills-content');
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
+/**
+   * Easy selector helper function
+   */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
   }
+}
 
+
+
+/**
+ * Skills animation
+ */
+let skilsContent = select('.skills-content');
+if (skilsContent) {
+  new Waypoint({
+    element: skilsContent,
+    offset: '80%',
+    handler: function(direction) {
+      let progress = select('.progress .progress-bar', true);
+      progress.forEach((el) => {
+        el.style.width = el.getAttribute('aria-valuenow') + '%'
+      });
+    }
+  })
+}
+
+
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  })
+});
+
+});
